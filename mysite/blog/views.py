40e42c0eceee8bfa8c0cfa45f7b1664a0af268ef
guang <av1254@qq.com>
2016-05-19 13:32:35 -0400

    index update

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
 # Create your views here.
+from .models import Post
+
+def post_list(request):
+    try:
+        posts = Post.objects.filter(published_date__isnull=False).order_by('-published_date')
+    except Exception as ex:
+        print ex,"-------------------"
+    return render(request, 'blog/post_list.html', {'posts':posts})
