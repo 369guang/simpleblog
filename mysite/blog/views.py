1cb7b412a72e2adfede34bc7f0656c78bb14d8de
guang <av1254@qq.com>
2016-05-25 06:54:40 -0400

    update

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,9 +1,27 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.core.urlresolvers import reverse
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .models import Post
 from .forms import PostForm
+from django.db.models import Count
 
 def post_list(request):
+    '''
+    postsAll = Post.objects.annotate(num_comment=Count('create_date')).filter(
+        published_date__isnull=False).prefetch_related(
+            'category').prefetch_related('tags').order_by('-published_date')
+    for p in postsAll:
+        p.click = cache_manager.get_click(p)
+    paginator = Paginator(postsAll, 10)
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+    return render(request, 'blog/post_list.html', {'posts':posts, 'page':True})
+    '''
     posts = Post.objects.filter(published_date__isnull=False).order_by('-published_date')
     return render(request, 'blog/post_list.html', {'posts':posts})
 
@@ -39,3 +57,13 @@ def post_edit(request, pk):
 def post_draft_list(request):
     posts = Post.objects.filter(published_date__isnull=True).order_by('-create_date')
     return render(request, 'blog/post_draft_list.html', {'posts':posts})
+
+def post_publish(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.published()
+    return redirect('blog.views.post_detail', pk=pk)
+
+def post_remove(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.delete()
+    return redirect('blog.views.post_list')
